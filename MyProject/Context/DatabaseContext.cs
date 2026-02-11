372cc81b959efffdee8fa3f02845522dbfb5cd58
TheMission15 <raulchinr@gmail.com>
2026-02-11 11:57:58 +0000

    Initial commit

---
+++ MyProject/Context/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using MyProject.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyProject.Context
+{
+    public class DatabaseContext : IdentityDbContext<User>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
+        {
+            var folder = Environment.SpecialFolder.MyDocuments;
+            var path = Environment.GetFolderPath(folder);
+            var dbPath = Path.Join(path, "database.db");
+            optionbuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
